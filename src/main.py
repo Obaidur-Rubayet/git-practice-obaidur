d287bac65e1e03a2bc7341e7af4e0486a6b97e05
Obaidur Rahman <obaidur143816@gmail.com>
2026-05-01 17:31:23 +0600

    Add error handling

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+import datetime
+
+print("Name: Obaidur Rahman")
+print("Today Date:", datetime.date.today())
+
 from utils import add, subtract
 
 print("Add:", add(5, 3))
 print("Subtract:", subtract(5, 3))
 
 print("Multiply:", multiply(5, 3))
+
+print("Divide:", divide(10, 2))
